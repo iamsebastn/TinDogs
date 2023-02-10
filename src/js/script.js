8745b957be132e7cf1c935ab9acf91a1aaa104de
Sebastian Wolf <sebastian.wolf298@gmail.com>
2023-02-10 11:14:49 +0100

    minor changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,26 @@
-import {dogs} from "./data.js"
-import {Dog} from "./dog.js"
+import { dogs } from "./data.js"
 import { renderSummary } from "./utils.js"
-
-const likesArray = []
-
+import Dog from "./Dog.js"
 
 
-const dogObj = new Dog(dogs)
+let likesArray = []
+
+function showSummary() {
+    const likedDogs = likesArray.map((dog) => {
+        return `
+        <img class="profile_img" src="${avatar}">
+        <div class="profile_desc">
+            <p class="p_head">${name}, ${age}</p>
+            <p class="p_body">${bio}</p>
+        </div>
+    `
+    })
+}
+
 function render() {
     document.getElementById("profile-thumb").innerHTML = dogObj.renderProfile(dogs[0])
 }
-
+const dogObj = new Dog(dogs)
 render()
 
 document.addEventListener("click", () => {
