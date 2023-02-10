fe78d719783418cac4ef713d861e8abe62ce4f1e
Sebastian Wolf <sebastian.wolf298@gmail.com>
2023-02-10 13:56:00 +0100

    minor changes

--- src/js/script.js
+++ src/js/script.js
@@ -1,13 +1,42 @@
 import { dogs } from "./data.js"
 import Dog from "./Dog.js"
 
-const likedArray = []
 let currentIndex = 0
 let dogObj = new Dog(dogs[currentIndex])
-console.log(dogObj)
 
+render()
+
+document.getElementById("like-btn").addEventListener("click", () => {
+    liked()
+    if(currentIndex === dogs.length) {
+        likedMatches()
+    }
+})
+    
 function render() {
     document.getElementById("profile-thumb").innerHTML = dogObj.getDogHtml()
 }
 
-render()
+function getNextDog() {
+    currentIndex++
+    dogObj = new Dog(dogs[currentIndex])
+    render()
+}
+
+function liked() {
+    let likeStamp = document.getElementById("like-stamp")
+    dogObj.hasBeenLiked = true
+    likeStamp.classList.toggle("opacity-low")
+    setTimeout(() => {
+        likeStamp.classList.toggle("opacity-low")
+        getNextDog()
+        render()
+    }, 1500)
+}
+
+function noped() {
+
+}
+
+
+
