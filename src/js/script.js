4c425aff81882e6780614deecf8b68a5b7885a4b
Sebastian Wolf <sebastian.wolf298@gmail.com>
2023-02-10 10:59:40 +0100

    export/import the files

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,10 @@
 import {dogs} from "./data.js"
 import {Dog} from "./dog.js"
+import { renderSummary } from "./utils.js"
 
 const likesArray = []
+
+
 
 const dogObj = new Dog(dogs)
 function render() {
@@ -10,16 +13,6 @@ function render() {
 
 render()
 
-// function renderSummary() {
-//     const likedSummary = likesArray.map((like) => {
-//         return `
-//         <div class="liked-obj">
-//             <img class="profile_img_s" src="images/dog-rex.jpg">
-//             <div>
-//                 <p class="p_head black">${like.name}, ${like.age}</p>
-//                 <p class="p_body">${like.bio}</p>
-//             </div>
-//         </div> 
-//         `
-//     })
-// }
+document.addEventListener("click", () => {
+    renderSummary()
+})
