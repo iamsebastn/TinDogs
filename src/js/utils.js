48dc7e3cdd844bc2aaca6ff9937c2061b20cd082
Sebastian Wolf <sebastian.wolf298@gmail.com>
2023-02-10 10:59:49 +0100

    export/import files

--- src/js/utils.js
+++ src/js/utils.js
@@ -1,1 +1,17 @@
 import { gsap } from "gsap";
+export { renderSummary }
+
+function renderSummary() {
+    // const likedSummary = likesArray.map((like) => {
+    //     return `
+    //     <div class="liked-obj">
+    //         <img class="profile_img_s" src="images/dog-rex.jpg">
+    //         <div>
+    //             <p class="p_head black">${like.name}, ${like.age}</p>
+    //             <p class="p_body">${like.bio}</p>
+    //         </div>
+    //     </div> 
+    //     `
+    // })
+    console.log("Click")
+}
